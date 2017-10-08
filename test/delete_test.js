32624d709cbae1373dd8a00b627d4322d54c61d6
an244 <thienan.le244@gmail.com>
2017-10-08 16:39:49 +0700

    4 methods to delete/remove

---
+++ test/delete_test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', ()=>{
+
+    beforeEach(async()=>{
+        Le = new User({name: 'Le'});
+        await Le.save();
+    });
+
+    //cach 1:
+    it('model instance remove', async()=>{
+        await Le.remove()
+         .then(()=> User.findOne({name: 'Le'}))
+         .then((userC1)=> assert(userC1 === null));
+    });
+
+    //cach 2:
+    it('class method remove', async()=>{
+    //remove a bunch of records with some given criteria
+    await User.remove({name: 'Le'})
+     .then(()=> User.findOne({name: 'Le'}))
+     .then((userC2)=> assert(userC2 === null));
+    });
+
+    //cach 3:
+    it('class method findOneAndRemove', async()=>{
+        await User.findOneAndRemove({name: 'Le'})
+        .then(()=> User.findOne({name: 'Le'}))
+        .then((userC3)=> assert(userC3 === null));   
+    });
+
+    //cach 4:
+    it('class method findbyIdAndRemove', async()=>{
+        await User.findByIdAndRemove(Le._id)
+        .then(()=> User.findOne({name: 'Le'}))
+        .then((userC4)=> assert(userC4 === null));  
+    });
+});
